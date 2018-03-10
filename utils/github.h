1dedf01122a556562fb2e30bed8ddb2f032802f4
Mikalai Ananenka <git@3dyd.com>
2018-03-10 16:54:31 +0300

    provide 'download_url' property

--- utils/github.h
+++ utils/github.h
@@ -22,8 +22,12 @@ struct github_conf {
     return static_api_ptr_t<http_client>()->create_request("GET");
   }
 
-  static bool is_acceptable(const rapidjson::Value& release) {
+  static bool is_acceptable_release(const rapidjson::Value& release) {
     return true;
+  }
+
+  static bool is_acceptable_asset(const rapidjson::Value& asset) {
+    return false;
   }
 };
 
@@ -56,8 +60,15 @@ class github_releases: public request {
     return url;
   }
 
-  virtual bool is_acceptable(const rapidjson::Value& release) {
-    return t_github_conf::is_acceptable(release);
+  virtual void process_asset(const rapidjson::Value& asset, file_info& info) {
+    rapidjson::StringBuffer buffer;
+    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+    asset.Accept(writer);
+    info.info_set_ex("asset", pfc_infinite, buffer.GetString(), buffer.GetLength());
+
+    if (asset.HasMember("browser_download_url") && asset["browser_download_url"].IsString()) {
+      info.meta_set("download_url", asset["browser_download_url"].GetString());
+    }
   }
 
   virtual void process_release(const rapidjson::Value& release, file_info& info) {
@@ -66,6 +77,16 @@ class github_releases: public request {
 
     ACFU_EXPECT_JSON(release.HasMember("html_url") && release["html_url"].IsString());
     info.meta_set("download_page", release["html_url"].GetString());
+
+    if (release.HasMember("assets") && release["assets"].IsArray()) {
+      auto assets = release["assets"].GetArray();
+      for (const auto& asset : assets) {
+        if (t_github_conf::is_acceptable_asset(asset)) {
+          process_asset(asset, info);
+          break;
+        }
+      }
+    }
 
     rapidjson::StringBuffer buffer;
     rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
@@ -77,7 +98,7 @@ class github_releases: public request {
     ACFU_EXPECT_JSON(json.IsArray());
     auto releases = json.GetArray();
     for (const auto& release : releases) {
-      if (is_acceptable(release)) {
+      if (t_github_conf::is_acceptable_release(release)) {
         return process_release(release, info);
       }
     }
@@ -96,7 +117,7 @@ class github_latest_release: public github_releases<t_github_conf> {
 
   virtual void process_response(const rapidjson::Value& json, file_info& info) {
     ACFU_EXPECT_JSON(json.IsObject());
-    if (is_acceptable(json)) {
+    if (t_github_conf::is_acceptable_release(json)) {
       process_release(json, info);
     }
   }
